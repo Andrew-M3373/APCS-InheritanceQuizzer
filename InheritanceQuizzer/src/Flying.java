fadb4c76d868ceb9e38880e7433c888d1699f32c
Andrew <Andrew@AComputer>
2021-12-17 04:58:06 -0700

    Completed

--- InheritanceQuizzer/src/Flying.java
+++
@@ -1,8 +0,0 @@
-public class Flying extends Hobby {
-    public static void haveFun() {
-        System.out.println("I'm having even more fun.");
-    }
-    public static void seeTheWorld() {
-        System.out.println("I get to see the world from above.");
-    }
-}
--- InheritanceQuizzer/src/Hobby.java
+++
@@ -1,8 +0,0 @@
-public class Hobby {
-    public static void haveFun() {
-        System.out.println("I'm having so much fun.");
-    }
-    public static void wasteTime() {
-        System.out.println("It may be fun, but is it useful?");
-    }
-}
